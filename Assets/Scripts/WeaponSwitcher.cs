ccb217f534f78cb8bc87d1c3d289ec24ebbd95dc
PastryChew <80216822+PastryChew@users.noreply.github.com>
2022-04-05 15:16:46 +0300

    pz 24

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    public int weaponSwitch = 0;
+
+    bool TakeShield = true;
+    public GameObject shield;
+
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    
+    void Update()
+    {
+        int currentWeapon = weaponSwitch;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (weaponSwitch >= transform.childCount - 1)
+            {
+                weaponSwitch = 0;
+            }
+            else
+            {
+                weaponSwitch++;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (weaponSwitch <= 0)
+            {
+                weaponSwitch = transform.childCount - 1;
+            }
+            else
+            {
+                weaponSwitch--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ShieldActive();
+        }
+        
+
+        if (currentWeapon != weaponSwitch)
+        {
+            SelectWeapon();
+        }
+    }
+
+    public void SelectWeapon()
+    {
+        int i=0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == weaponSwitch)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            
+            i++;
+        }
+    }
+
+    public void ShieldActive()
+    {
+        if (TakeShield)
+        {
+            shield.SetActive(false);
+            TakeShield = false;
+        }
+        else
+        {
+            shield.SetActive(true);
+            TakeShield = true;
+        }
+    }
+}
