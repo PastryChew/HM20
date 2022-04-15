35155aa34ec62b1b0b577c4cf69c474155737538
PastryChew <80216822+PastryChew@users.noreply.github.com>
2022-04-15 16:57:23 +0300

    CutScene

--- Assets/Scripts/StartTimeline.cs
+++ Assets/Scripts/StartTimeline.cs
@@ -2,30 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class StartTimeline : MonoBehaviour
 {
     public PlayableDirector timeline;
-    public bool check = true;
+    public GameObject player;
+
+    bool playScene = false;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         timeline = GetComponent<PlayableDirector>();
-        check = GetComponent<PlayerCheck>().cutscene;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.E) && check)
+        if (Input.GetKeyDown(KeyCode.E) && player.gameObject.GetComponent<PlayerCheck>().cutscene)
         {
+            player.gameObject.GetComponent<ThirdPersonUserControl>().enabled = false;
+
+            player.transform.localPosition = Vector3.zero;
+            player.transform.localRotation = Quaternion.Euler(0, 90, 0);
             timeline.Play();
+            playScene = true;
         }
+
+        if (timeline.state != PlayState.Playing && playScene)
+        {
+            playScene = false;
+            player.gameObject.GetComponent<ThirdPersonUserControl>().enabled = true;
+        }
+       
     }
 
-   
-    
 }
